cb0e28ed87420075fa10ed3e1b560ac5896f4a09
Gabriel Henriques <dev.gabrielhenriques@gmail.com>
2020-06-05 12:22:41 -0400

    feat: add migration for character model

--- lotr_v2/models.py
+++ lotr_v2/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class Character(models.Model):
+
     name = models.CharField(max_length=30)
-    gender = models.CharField(max_length=10),
-    birth = models.CharField(max_length=15),
-    death = models.CharField(max_length=15),
-    hair = models.CharField(max_length=30),
-    height = models.CharField(max_length=30),
-    race = models.CharField(max_length=30),
-    realm = models.CharField(max_length=50),
-    spouse = models.CharField(max_length=40),
+    gender = models.CharField(max_length=10)
+    birth = models.CharField(max_length=15)
+    death = models.CharField(max_length=15)
+    hair = models.CharField(max_length=30)
+    height = models.CharField(max_length=30)
+    race = models.CharField(max_length=30)
+    realm = models.CharField(max_length=50)
+    spouse = models.CharField(max_length=40)
