a183def946293f828f167f2105740bc57954219c
Gabriel Henriques <dev.gabrielhenriques@gmail.com>
2020-06-05 12:05:37 -0400

    feat: add character model

--- lotr_v2/models.py
+++ lotr_v2/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Character(models.Model):
+    name = models.CharField(max_length=30)
+    gender = models.CharField(max_length=10),
+    birth = models.CharField(max_length=15),
+    death = models.CharField(max_length=15),
+    hair = models.CharField(max_length=30),
+    height = models.CharField(max_length=30),
+    race = models.CharField(max_length=30),
+    realm = models.CharField(max_length=50),
+    spouse = models.CharField(max_length=40),
