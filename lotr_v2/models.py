202a9819c05d61b926041e126e72f29ffcbc9b1d
Gabriel Henriques <dev.gabrielhenriques@gmail.com>
2020-06-06 11:33:59 -0400

    update character model

--- lotr_v2/models.py
+++ lotr_v2/models.py
@@ -3,12 +3,22 @@ from django.db import models
 
 class Character(models.Model):
 
-    name = models.CharField(max_length=30)
-    gender = models.CharField(max_length=10)
-    birth = models.CharField(max_length=15)
-    death = models.CharField(max_length=15)
-    hair = models.CharField(max_length=30)
-    height = models.CharField(max_length=30)
-    race = models.CharField(max_length=30)
-    realm = models.CharField(max_length=50)
-    spouse = models.CharField(max_length=40)
+    name = models.CharField(max_length=60, default=None)
+
+    height = models.FloatField(null=True, blank=True, default=None)
+
+    race = models.CharField(max_length=20, default=None)
+
+    gender = models.CharField(max_length=10, default=None)
+
+    birth = models.CharField(max_length=30, default=None)
+
+    spouse = models.CharField(max_length=70, default=None)
+
+    death = models.CharField(max_length=30, default=None)
+
+    realm = models.CharField(max_length=90, default=None)
+
+    hair = models.CharField(max_length=20, default=None)
+
+    wiki = models.CharField(max_length=90, default=None)
