d460550d0c96f9d5c67360b617dff56d780e9b5a
Gabriel Henriques <dev.gabrielhenriques@gmail.com>
2020-06-10 14:05:26 -0400

    feat: register api urls

--- lotr_api/urls.py
+++ lotr_api/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include, url
 from django.urls import path
+from lotr_v2 import urls as lotr_v2_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r"^", include(lotr_v2_urls)),
 ]
